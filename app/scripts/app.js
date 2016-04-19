6bc2aad0bcb8ec957f40776284170010be44ad7e
onasenko-sergey <onasenko.sergey@gmail.com>
2016-04-19 22:56:16 +0300

    prevent carousel from autorunning

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,14 @@
 // import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
+import 'bootstrap-styl/js/carousel';
+import 'bootstrap-styl/js/transition';
+
 $(() => {
 	// svg4everybody();
+
+	// Stop carousels from autorunning
+	$('.carousel').carousel({
+		interval: false
+	});
 });
