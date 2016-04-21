b4b3173364436c9bbb360b17872c00e9f464cd42
onasenko-sergey <onasenko.sergey@gmail.com>
2016-04-21 17:11:50 +0300

    replace urls in inlined css('..' ->  'assets')

--- tasks/inline.js
+++ tasks/inline.js
@@ -1,8 +1,23 @@
 import gulp from 'gulp';
 import inline from 'gulp-inline-source';
+import replace from 'gulp-replace';
+import runSequence from 'run-sequence';
 
-	gulp.task('inline', () => {
-		gulp.src('dist/index.html')
-			.pipe(inline())
-			.pipe(gulp.dest('dist'));
-		});
+gulp.task('inline', () => {
+	runSequence(
+		'replace',
+		'inline:css'
+	);
+});
+
+gulp.task('replace', () => {
+	return gulp.src('dist/assets/styles/inline.min.css')
+		.pipe(replace('..', 'assets'))
+		.pipe(gulp.dest('dist/assets/styles'));
+});
+
+gulp.task('inline:css', () => {
+	return gulp.src('dist/index.html')
+		.pipe(inline())
+		.pipe(gulp.dest('dist'));
+});
