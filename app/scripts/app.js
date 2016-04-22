43937890cdcdc4e03556db5ad0ce3c6174adb681
onasenko-sergey <onasenko.sergey@gmail.com>
2016-04-23 00:57:19 +0300

    deferred background loading

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,11 +5,26 @@ import 'bootstrap-styl/js/carousel';
 import 'bootstrap-styl/js/transition';
 import 'bootstrap-styl/js/collapse';
 
+import blazy from 'blazy';
+
 $(() => {
 	// svg4everybody();
+
 
 	// Stop carousels from autorunning
 	$('.carousel').carousel({
 		interval: false
 	});
+
+
+	// Lazyload images
+	const loadOffset = 250;
+	/* eslint-disable */
+	// add "bg-load-on" class to elements when they are almost visible
+	new blazy({
+		errorClass: 'bg-load-on',
+		offset: loadOffset,
+		selector: '.bg-load'
+	});
+	/* eslint-enable */
 });
